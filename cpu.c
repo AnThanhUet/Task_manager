6bb562687860cef67f4a6291f3965eeb359017c0
AnThanh <theanuet@gmail.com>
2020-10-10 15:00:04 +0700

    add

--- cpu.c
+++ cpu.c
@@ -2,48 +2,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 FILE *fp = NULL;
-
-struct rootDir
+struct cpu 
 {
-    char shortName[9];
-    char shortExtension[4];
-    uint8_t attributes;
-    int sizeOffile;
-    uint16_t timeCre;
-    uint16_t dateCre;
+    char m_cpu[100];
 };
+struct cpu a;
 
-int displayFiles(struct rootDir files)
+
+void read_cpu()
 {
-    printf("%-8s\t",files.shortName);                                                                        // Short name
-    printf("%-5s\t",files.shortExtension);                                                                    // Extension
-    printf("%-6d\t",(files.attributes>>4)&1);                                                                // SubDir mask
-    if(!((files.attributes>>4)&1))                                                                            // Check: Subdirectory or file
+    
+    fp = fopen("text.txt", "r");
+    if(fp != NULL)
     {
-        printf("%d-%.2d-%d\t", (files.dateCre>>9)+1980, (files.dateCre&0x01E0)>>5, (files.dateCre&0x001F));    // Created date (year-month-day)
-        printf("%d:%d:%d", files.timeCre>>11, (files.timeCre&0x07E0)>>5, 2*(files.timeCre&0x001F));         // Created time (Hour:min:sec)
+        fscanf(fp, "\n\n\n%s", &a.m_cpu);
+        printf("%s", a.m_cpu);
+        fclose(fp);
     }
-    printf("\n");
+    else
+        printf("Can't open file !");
 }
 
-void main(int argc, char **argv) {
-    int pid;
-    sscanf(argv[1], "%d", &pid);
-    printf("pid = %d\n", pid);
 
-    char filename[1000];
-    sprintf(filename, "/proc/%d/stat", pid);
-    FILE *f = fopen(filename, "r");
-
-    int unused;
-    char comm[1000];
-    char state;
-    int ppid;
-    fscanf(f, "%d %s %c %d", &unused, comm, &state, &ppid);
-    printf("comm = %s\n", comm);
-    printf("state = %c\n", state);
-    printf("parent pid = %d\n", ppid);
-    fclose(f);
+int main() 
+{
+    read_cpu;
+    return 0;
 }
